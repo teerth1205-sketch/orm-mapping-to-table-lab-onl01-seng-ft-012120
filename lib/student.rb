f2851ede6554f324f25b98b089283978e036afd7
Teerth Patel <teerth1205@gmail.com>
2020-02-22 03:21:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,6 +21,12 @@ def self.create_table
   
 end 
 
+def self.drop_table 
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+end 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
