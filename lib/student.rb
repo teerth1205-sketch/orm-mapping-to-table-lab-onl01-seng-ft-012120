9baa166a0c1a27813f0c3252002ea1d6ccd9be92
Teerth Patel <teerth1205@gmail.com>
2020-02-22 03:07:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ def initialize(name, grade, id = nil)
 end 
 
 def self.create_table
-  
+  sql = 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
