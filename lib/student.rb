eb75e2fadeee2fe92d052574570e3a400deb0d23
Teerth Patel <teerth1205@gmail.com>
2020-02-22 03:09:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,6 +17,10 @@ def self.create_table
     grade TEXT
     )
   SQL
+  DB[:conn].execute(sql)
+  
+end 
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
