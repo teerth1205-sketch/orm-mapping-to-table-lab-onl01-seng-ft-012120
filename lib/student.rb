a104c3625761137df5f06b695b596820d4d08b0c
Teerth Patel <teerth1205@gmail.com>
2020-02-22 03:35:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,10 +3,10 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
 
-def initialize(name, grade)
+def initialize(name, grade, id = nil)
   @name = name 
   @grade = grade
- 
+  @id = id 
 end 
 
 def self.create_table
