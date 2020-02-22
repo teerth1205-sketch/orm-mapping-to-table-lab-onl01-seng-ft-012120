4bcb069ffcd10f94502905f6a94665363a3e9084
Teerth Patel <teerth1205@gmail.com>
2020-02-22 03:11:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,8 +12,8 @@ end
 def self.create_table
   sql = <<- SQL 
   CREATE TABLE students (
-    id INTEGER PRIMARY KEY
-    name TEXT
+    id INTEGER PRIMARY KEY,
+    name TEXT,
     grade TEXT
     )
   SQL
